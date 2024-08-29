66f93c27756ccb677bb4e352b81d54f02b2ef80a
v2k2000 <shiun147@gmail.com>
2024-08-29 15:08:35 +0900

    hbase crus

---
+++ main.py
@@ -0,0 +1,112 @@
+from fastapi import FastAPI
+import happybase
+from pydantic import BaseModel
+from datetime import datetime
+
+app =  FastAPI()
+
+connection = happybase.Connection('localhost')
+connection.open()
+
+
+class User(BaseModel):
+    username: str
+    email: str
+
+class Chatroom(BaseModel):
+    room_name: str
+
+class Chat(BaseModel):
+    user_id: str
+    room_id: str
+    message: str
+
+
+
+# @app.get('/')    # urls
+# def read_root():  #views
+#     return {'hello':'world'}
+
+# @app.get('/index')
+# def index():
+#     return {'asdf':'asdf'}
+
+# @app.get('/items/{item_id}')
+# def read_item(item_id: int):
+#     return {'item_id':item_id}
+
+@app.post('/user')
+def create_user(user: User):
+    table = connection.table('user')
+    user_id = user.username 
+    table.put(
+        user_id,
+        {
+            'info:username':user.username,
+            'info:email': user.email
+        }
+    )
+
+    return {
+        'user_id': user_id,
+        'username': user.username,
+        'email': user.email
+    }
+
+@app.get('/user/{user_id}')
+def get_user(user_id: str):
+    table = connection.table('user')
+    row = table.row(user_id)
+
+    return {
+        'user_id': user_id,
+        'username': row[b'info:username'].decode('utf-8'),
+        'email': row[b'info:email'].decode('utf-8'), 
+    }
+
+
+@app.post('/chatroom')
+def create_chatroom(chatroom: Chatroom):
+    table = connection.table('chatroom')
+    chatroom_id = chatroom.room_name
+    table.put(chatroom_id, {'info:room _name':chatroom.room_name})
+    return { 'chatroom_id': chatroom_id, 'room_name':chatroom.room_name}
+
+@app.post('/chat')
+def create_chat(chat: Chat):
+    table = connection.table('chat')
+    timestamp = datetime.now().timestamp()
+    MAX_TIMESTAMP = 2**32 - 1
+
+    chat_id = f'{chat.room_id}_{MAX_TIMESTAMP-timestamp}'
+    table.put(chat_id, {
+        'info:user_id': chat.user_id,
+        'info:room_id': chat.room_id,
+        'info:nessage': chat.message,
+    })
+
+    return {
+        'chat_id': chat_id,
+        'user_id': chat.user_id,
+        'room_id': chat.room_id,
+        'massage': chat.message,
+    }
+
+@app.get('/chatroom/{room_id}')
+def get_chatroom(room_id: str):
+    table = connection.table('chat')
+
+    table.scan(filter=f"SingleColumnValueFilter('info', 'room_id', =, 'binary:{room_id}')")
+    
+    
+    chat = []
+
+    for k,v in rows:
+        chats.append({
+            'chat_id': k,
+            'room_id': v[b'info:room_id'],
+            'user_id': v[b'info:user_id'],
+            'massage': v[b'info:massage'],
+        })
+
+    return chats
